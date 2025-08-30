eed94f0c6eb49770dc94535f1ce14c4548a46168
Yash8141 <yashsolanki1622@gmail.com>
2025-08-30 16:45:27 +0200

    Github data added

--- index.js
+++ index.js
@@ -2,7 +2,45 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-
+const githubData = {
+  login: "hiteshchoudhary",
+  id: 11613311,
+  node_id: "MDQ6VXNlcjExNjEzMzEx",
+  avatar_url: "https://avatars.githubusercontent.com/u/11613311?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/hiteshchoudhary",
+  html_url: "https://github.com/hiteshchoudhary",
+  followers_url: "https://api.github.com/users/hiteshchoudhary/followers",
+  following_url:
+    "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  gists_url: "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  subscriptions_url:
+    "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  organizations_url: "https://api.github.com/users/hiteshchoudhary/orgs",
+  repos_url: "https://api.github.com/users/hiteshchoudhary/repos",
+  events_url: "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/hiteshchoudhary/received_events",
+  type: "User",
+  user_view_type: "public",
+  site_admin: false,
+  name: "Hitesh Choudhary",
+  company: null,
+  blog: "https://hitesh.ai",
+  location: "India",
+  email: null,
+  hireable: null,
+  bio: "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  twitter_username: "hiteshdotcom",
+  public_repos: 112,
+  public_gists: 5,
+  followers: 48747,
+  following: 0,
+  created_at: "2015-03-23T13:03:25Z",
+  updated_at: "2025-08-24T19:12:57Z",
+};
 // Add middleware for better performance
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,6 +76,11 @@ app.get("/youtube", (req, res) => {
   res.send("<h2>Chai aur code</h2>");
 });
 
+app.get("/github", (req, res) => {
+  res.json(githubData);
+  // res.send("GITHUB");
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
@@ -53,5 +96,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Your app is running on port number ${PORT}`);
-  console.log(`Health check available at: http://localhost:${PORT}/health`);
+  // console.log(`Health check available at: http://localhost:${PORT}/health`);
 });
